85f51468c95bf212441ae4c450f01d71f3a7346d
yashvichauhan <31180761+yashvichauhan@users.noreply.github.com>
2023-02-04 01:42:05 -0500

    adding pages and views

--- routes/index.js
+++ routes/index.js
@@ -13,22 +13,26 @@ router.get('/home', function(req, res, next) {
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('about', { title: 'About'});
 });
 
 /* GET Products page. */
 router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+  res.render('products', { title: 'Products'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+  res.render('services', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('contact', { title: 'Contact'});
+});
+
+router.get('*', function(req, res){
+  res.render('error', { message: 'Error 404'});
 });
 
 module.exports = router;
